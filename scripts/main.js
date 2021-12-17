7ae805d0271ce4996ac6c5984089b0be78f9ec92
Kepes Tamas <kepes.tamas@danubiusinfo.hu>
2021-12-17 10:27:15 +0100

    Add cookie bar

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const Helper = (function () {
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+  return {
+    getCookie,
+  };
+})();
